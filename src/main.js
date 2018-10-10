f85163e12dec91bf29ff7885fabe9d2afa4dd70e
TaekyunKim <tuhbm2@gmail.com>
2018-10-10 16:57:15 +0900

    before real database binding

--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index';
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,
-  store
+  store,
+  created () {
+      firebase.initializeApp({
+          apiKey: "AIzaSyCLnECG5N1NAVw3fmEuwEKKjHfXt3w5lPE",
+          authDomain: "vue-project-56bfb.firebaseapp.com",
+          databaseURL: "https://vue-project-56bfb.firebaseio.com",
+          projectId: "vue-project-56bfb",
+          storageBucket: "vue-project-56bfb.appspot.com",
+          messagingSenderId: "561682636729"
+      })
+      firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+              this.$store.dispatch('autoSignIn', user) // 로그인 유지하기
+              this.$store.dispatch('fetchUserData') // 로그인 유지하기
+          }
+      })
+      this.$store.dispatch('loadMeetups')
+  }
 }).$mount('#app')
